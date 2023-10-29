dedc5ba4368a5bf0da87920f11808706925d5ce1
lia-gat <114720640+lia-gat@users.noreply.github.com>
2023-10-29 21:17:19 +0500

    04
    21.09.2023

---
+++ 04/07.py
@@ -0,0 +1,14 @@
+num = int(input('Enter number: '))
+a = 0
+
+for i in range(1, num+1):
+    if (num % i == 0):
+        print(i, end=' ')
+        a +=1
+
+print('\n') 
+if a == 2:
+    print('ПРОСТОЕ')
+else:
+    print('НЕТ')
+    
---
+++ 04/10.py
@@ -0,0 +1,11 @@
+numbers = int(input('Enter numbers: '))
+
+for i in range(numbers):
+    num = int(input('Enter num: '))
+    if (i % num == 0):
+        print('YES')
+    else:
+        print('NO')
+ 
+    
+        
