746acca000554cd62cbb86c6c919a288e7ebfbb4
lia-gat <114720640+lia-gat@users.noreply.github.com>
2023-11-20 09:35:28 +0500

    17

---
+++ 17/05.py
@@ -0,0 +1,15 @@
+def mirror(arr):
+    mirrored_part =  arr.copy()
+    mirrored_part.reverse()
+    arr += mirrored_part
+    return arr
+    
+arr = [1, 2]
+mirror(arr)
+print(*arr)
+
+print('*****')
+
+arr = [1]
+mirror(arr)
+print(*arr)
